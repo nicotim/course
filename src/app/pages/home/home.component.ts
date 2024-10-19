8acdce898872faba31ec53a3e356a148af495f70
nicotim <nik21timo@gmail.com>
2024-10-19 16:19:32 -0300

    Home page almost done

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
+
+const MODULES = [MatToolbarModule, MatIconModule, MatListModule, RouterModule];
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MODULES],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  list = [
+    { name: 'Sign up', isActive: false },
+    { name: 'Sign in', isActive: false },
+  ];
 }
